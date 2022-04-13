c68281d355416c728a68170513988bb63cd7fb1c
Boone B Gorges <boonebgorges@gmail.com>
2022-04-13 14:57:43 -0500

    1.4.5 version bumps.

--- wp-grade-comments.php
+++ wp-grade-comments.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP Grade Comments
-Version: 1.4.4
+Version: 1.4.5
 Description: Grades and private comments for WordPress blog posts. Built for the City Tech OpenLab.
 Author: Boone Gorges
 Author URI: http://boone.gorg.es
